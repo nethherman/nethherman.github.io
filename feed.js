309e49878edfd4215189c622c593f5dbb902da96
Herman Nz <31732270+nethherman@users.noreply.github.com>
2017-09-18 11:29:14 +0700

    Create feed.js

---
+++ feed.js
@@ -0,0 +1,8 @@
+document.write("\x3cdiv class\x3d\x22feedburnerFeedBlock\x22 id\x3d\x22DistributorRoseVIndonesiaaudtqqtic6bii8sktc4a4et5q0\x22\x3e");
+document.write("\x3cul\x3e");
+document.write("\x3cli\x3e\x3cspan class\x3d\x22headline\x22\x3e\x3ca href\x3d\x22http://www.geropos.com/2017/09/membuat-gambar-gifs-dari-video-di-linux.html\x22 target\x3d\x22_blank\x22 \x3eMembuat Gambar GIFs Dari Video Di Linux Dengan Gifcurry\x3c/a\x3e\x3c/span\x3e");
+document.write("\x3cdiv\x3e\x3c/div\x3e");
+document.write("\x3c/li\x3e");
+document.write("\x3c/ul\x3e");
+document.write(" \x3cdiv id\x3d\x22creditfooter\x22\x3e\x3ca href\x3d\x22https://feedburner.google.com\x22 target\x3d\x22_blank\x22\x3e\x3cimg src\x3d\x22//feedburner.google.com/fb/images/buzzboost-pwrd.gif\x22 alt\x3d\x22Headlines by FeedBurner\x22 style\x3d\x22border:0\x22/\x3e\x3c/a\x3e\x3c/div\x3e");
+document.write("\x3c/div\x3e");
